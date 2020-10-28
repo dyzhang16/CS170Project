9769ce1f36c4db276cd425b14365ce18a395fb4b
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2020-10-28 16:45:54 -0700

    Added Scenes for Dialogue and Maze

---
+++ Assets/Scripts/DialogueSceneChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.SceneManagement;  
+
+public class DialogueSceneChanger : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+        void onMouseDown()
+    {
+         SceneManager.LoadScene("Scene3");                  //https://forum.unity.com/threads/how-to-load-a-scene-with-a-button.508078/
+    }
+}
