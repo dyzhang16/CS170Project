d72c700951ea800aba26201bf7898eabee787af2
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2020-10-30 11:50:37 -0700

    Added WASD Movement and Collision Detection

---
+++ Assets/Scripts/ChangeMainScene2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeMainScene2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D collider)                          //https://www.youtube.com/watch?v=Bc9lmHjqLZc
+    {
+        if(collider.gameObject.CompareTag("Player")){
+            SceneManager.LoadScene("Scene1");
+            //Debug.Log("Collision Detected.");
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour                                     //https://www.youtube.com/watch?v=rxyvB-97h4I
+{
+    public float speed = 10.4f;
+    
+    public void Update()
+    {
+        Vector3 pos = transform.position;
+
+        if(Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime; //for 3D can use pos.z
+        }
+        if(Input.GetKey("s"))
+        {
+             pos.y -= speed * Time.deltaTime;
+        }
+        if(Input.GetKey("d"))
+        {
+             pos.x += speed * Time.deltaTime;
+        }
+        if(Input.GetKey("a"))
+        {
+             pos.x -= speed * Time.deltaTime;
+        }
+
+        transform.position = pos;
+    }
+}
