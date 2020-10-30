e05c5f7e0e240e211cf8f1c91621d1db9c2a88d6
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2020-10-29 18:54:52 -0700

    Added switching Scenes

---
+++ Assets/Scripts/ChangeMatchScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeMatchScene : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+}
