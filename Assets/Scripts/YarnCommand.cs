4c8ff1bbe88f359789c5f1b6aa436425902d0952
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2021-01-05 17:57:44 -0800

    Added Custom Yarn Commands
    Added Custom Yarn Commands,
    Open Puzzle through Yarn
    Way to track Nodes through Environment

---
+++ Assets/Scripts/YarnCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class YarnCommand : MonoBehaviour
+{
+    public GameObject puzzlePanel;
+
+    [YarnCommand("Show")]
+    public void ShowPuzzle()
+    {
+        puzzlePanel.GetComponent<CanvasGroup>().alpha = 1;
+    }
+
+}
