9c34857ede1cd0eb538bb1d6cb7b86845a0b714f
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2021-01-06 16:52:06 -0800

    Added NPC movement through YarnCommands

--- Assets/Scripts/Levels/Tutorial/Yarn/YarnCommand.cs
+++ Assets/Scripts/Levels/Tutorial/Yarn/YarnCommand.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Yarn.Unity;
 
 public class YarnCommand : MonoBehaviour
 {
     public GameObject puzzlePanel;
+    public string sceneToChange;
+    public bool isWalking;
+
 
     [YarnCommand("Show")]
     public void ShowPuzzle()
     {
         puzzlePanel.GetComponent<CanvasGroup>().alpha = 1;
+    }
+    [YarnCommand("ChangeScene")]
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneToChange);
+    }
+    [YarnCommand("MoveNPC")]
+    public void MoveNPC()
+    {
+        isWalking = true;
     }
 
 }
---
+++ Assets/Scripts/NPC/isMoving.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isMoving : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Character;
+    public GameObject Destination;
+    public float Speed;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Character.GetComponent<YarnCommand>().isWalking)
+        {
+            Debug.Log("Movin");
+            Vector3 distance = Character.transform.position - Destination.transform.position;
+            distance = -distance.normalized;
+            Character.transform.position += distance * Speed * Time.deltaTime;
+            if (distance.y >= 0 && distance.x >= 0)          //Changes based on Positioning
+            {
+                Character.GetComponent<YarnCommand>().isWalking = false;
+            }
+        }
+    }
+}
