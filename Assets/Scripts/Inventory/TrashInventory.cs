2ebcebf1eeca05c70b753b4cf05d6b745a431244
Emmanuel Butor <ebutor@ucsc.edu>
2021-01-19 22:30:01 -0800

    Inventory Trash Implementation
    Summary: Added the ability for the player to trash items in their
    inventory by dragging said item into the trash. Also added a single line
    in .gitignore because packages-lock.json was still being tracked.
    
    CoffeeScene.unity
    - Added a new GameObject in InventoryPanel called "Trash Slot", which
    itself contains a GameObject called "Trash Image" which does the actual
    handling of trashing things.
    
    TrashInventory.cs
    - Handles all of the trashing operations and also has the trash icon
    toggle between whether it is opened or closed.
    - This also runs some dialogue and has a YarnCommand that confirms the
    action of trashing the Item.
    
    Cup.yarn
    - Added two new nodes: TrashItem and TrashIt
    - TrashItem asks the player if they want to throw the item away.
    - TrashIt is run if the player wishes to throw the item away. It calls
    the YarnCommand "Trash" with "Confirm" as parameter 0
    
    Miscellaneous
    - Added two placeholder trash .pngs that should be replaced

---
+++ Assets/Scripts/Inventory/TrashInventory.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Yarn.Unity;
+
+public class TrashInventory : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IDropHandler
+{
+	// dialogue runner
+	public DialogueRunner dialogueRunner;
+
+	// current item ready to trash
+	private Item itemToTrash;
+
+	// Images for trash open and trash closed
+	public Sprite trashOpened = null;
+	public Sprite trashClosed = null;
+
+	// affects what Image should be shown
+	private bool showOpenTrash = false;
+	
+	void Awake()
+	{
+		// add the trash command to DialogueRunner
+		if (dialogueRunner)
+		{
+			dialogueRunner.AddCommandHandler(
+				"Trash",
+				DoTrashItem
+			);
+		}
+	}
+
+	void Update()
+	{
+		if (showOpenTrash)
+		{
+			GetComponent<UnityEngine.UI.Image>().sprite = trashOpened;
+		}
+		else
+		{
+			GetComponent<UnityEngine.UI.Image>().sprite = trashClosed;
+		}
+	}
+
+	// Below two functions handle when the trash sprite should be opened or closed
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		if (eventData.dragging)
+		{
+			// Checks if the dragged item contains this Component (to make sure no other draggable objects trigger this)
+			ItemDragHandler dragHandler = eventData.pointerDrag.GetComponent<ItemDragHandler>();
+			if (dragHandler)
+			{
+				showOpenTrash = true;
+			}
+		}
+	}
+	public void OnPointerExit(PointerEventData eventData)
+	{
+		// No special checks here, as when the pointer exits the trash should be closed
+		showOpenTrash = false;
+	}
+
+
+	// Below function handles when an Item is dropped onto the Trash Slot
+	public void OnDrop(PointerEventData eventData)
+	{
+		if (dialogueRunner && !dialogueRunner.IsDialogueRunning)
+		{
+			// line of code used from InventorySlot.OnDrop
+			itemToTrash = Inventory.instance.itemList[eventData.pointerDrag.GetComponent<ItemDragHandler>().transform.parent.GetSiblingIndex()];
+			dialogueRunner.StartDialogue("TrashItem");
+		}
+		// Set trash to be closed
+		showOpenTrash = false;
+	}
+
+	// Function that will trash the item
+	private void DoTrashItem(string[] parameters)
+	{
+		if (parameters[0] == "Confirm" && itemToTrash)
+		{
+			Inventory.instance.RemoveItem(itemToTrash);
+			itemToTrash = null;
+		}
+	}
+}
