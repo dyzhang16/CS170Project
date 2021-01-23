f506702cdff3e88539d08e34b71ddc76dbf31369
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2021-01-23 12:14:21 -0800

    Added new Assets Linked Yarnspinner to new assets
    Added Recipe Book
    Fixed Cream and Sugar Puzzle
    Added Note to John
    Linked all these assets with yarnspinner

--- Assets/Scripts/Levels/coffee level/creamPuzzle.cs
+++ Assets/Scripts/Levels/coffee level/creamPuzzle.cs
@@ -4,49 +4,23 @@ using UnityEngine;
 
 public class creamPuzzle : MonoBehaviour
 {
-    public GameObject creamPuzzlePanel, coffeeUI, creamUI, sweetnerUI, sugar, cream;
-    public int creamAdded, sweetnerAdded = 0;
-    public bool sweetShaking, creamShaking = false;
-    
-    public void Start(){
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    public GameObject creamPuzzlePanel, coffeeUI, creamUI, cream;
+    public int creamAdded = 0;
+    public bool creamShaking;
 
     public void addCream(){
         if (!creamShaking){
             creamShaking = !creamShaking;
-            creamUI.transform.Rotate(0, 0, -35);
+            creamUI.transform.Rotate(0, 0, 35);
 
             ++creamAdded;
-            Vector3 pos = new Vector3(creamUI.transform.position.x + 70, creamUI.transform.position.y, creamUI.transform.position.z);
+            Vector3 pos = new Vector3(creamUI.transform.position.x - 70, creamUI.transform.position.y, creamUI.transform.position.z);
             GameObject creams = Instantiate(cream, pos, creamUI.transform.localRotation, transform);
         }
         else
         {
             creamShaking = !creamShaking;
-            creamUI.transform.Rotate(0, 0, 35);
-        }
-    }
-
-    public void addSweetner(){
-
-        if (!sweetShaking) {
-            sweetShaking = !sweetShaking;
-            sweetnerUI.transform.Rotate(0, 0, 35);
-
-            ++sweetnerAdded;
-            Vector3 pos = new Vector3(sweetnerUI.transform.position.x - 70, sweetnerUI.transform.position.y, sweetnerUI.transform.position.z);
-            GameObject sugs = Instantiate(sugar, pos, sweetnerUI.transform.localRotation, transform);
-        }
-        else
-        {
-            sweetnerUI.transform.Rotate(0, 0, -35);
-
-            sweetShaking = !sweetShaking;
+            creamUI.transform.Rotate(0, 0, -35);
         }
     }
 }
---
+++ Assets/Scripts/Levels/coffee level/sugarPuzzle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sugarPuzzle : MonoBehaviour
+{
+    public GameObject sugarPuzzlePanel, coffeeUI, sugarUI, sugar;
+    public int sugarAdded = 0;
+    public bool sweetShaking;
+    public void addSugar()
+    {
+
+        if (!sweetShaking)
+        {
+            sweetShaking = !sweetShaking;
+            sugarUI.transform.Rotate(0, 0, -35);
+
+            ++sugarAdded;
+            Vector3 pos = new Vector3(sugarUI.transform.position.x + 70, sugarUI.transform.position.y, sugarUI.transform.position.z);
+            GameObject sugs = Instantiate(sugar, pos, sugarUI.transform.localRotation, transform);
+        }
+        else
+        {
+            sugarUI.transform.Rotate(0, 0, 35);
+
+            sweetShaking = !sweetShaking;
+        }
+    }
+}
