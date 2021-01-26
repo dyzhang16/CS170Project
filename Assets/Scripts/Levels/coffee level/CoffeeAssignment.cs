92c1c606caae5870a65e616f8c48b25a1185b5d6
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2021-01-25 19:52:59 -0800

    Finished Sugar/Cream Puzzle(?)
    Finished the sugar and cream puzzle

---
+++ Assets/Scripts/Levels/coffee level/CoffeeAssignment.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeAssignment : MonoBehaviour
+{
+    public Item Coffee1;
+    public Item Coffee2;
+    public Item Coffee3;
+    public Item Coffee4;
+    public Item Coffee5;
+    public Item droppedCoffee;
+    // Start is called before the first frame update
+
+}
