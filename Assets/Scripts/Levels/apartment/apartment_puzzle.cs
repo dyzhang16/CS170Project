0fa59e9d536369d11ac526b20da0c3b6de978559
Ly Phung <lynphung@ucsc.edu>
2021-02-04 00:34:52 -0800

    apartment level prototype completed

---
+++ Assets/Scripts/Levels/apartment/apartment_puzzle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class apartment_puzzle : MonoBehaviour
+{
+
+    public GameObject[] packingList = new GameObject[0];
+    public Dictionary<string, bool> dic = new Dictionary<string, bool>();
+
+    public bool puzzleComplete;
+    public VariableStorageBehaviour CustomVariableStorage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (GameObject obj in packingList){
+            dic.Add(obj.name, false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void checkIfComplete(){
+        foreach(bool key in dic.Values){
+            if (!key){
+                puzzleComplete = false;
+                return;
+            }
+        }
+
+        puzzleComplete = true;
+        CustomVariableStorage.SetValue("$apartment_puzzle", 1);
+    }
+}
---
+++ Assets/Scripts/Levels/apartment/floor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class floor : MonoBehaviour, IDropHandler
+{
+
+    public apartment_puzzle puzzle;
+
+    // IN ORDER FOR THIS TO WORK YOU MUST
+    // https://answers.unity.com/questions/1161275/can-i-make-a-non-ui-gameobject-draggable-by-implem.html 
+    // 1. add the Physics Raycaster component to your main camera
+    // 2. have an EventSystem in your scene
+    // 3. change camera to tagged as "MainCamera"
+    public void OnDrop(PointerEventData eventData){
+        Item droppedItem = Inventory.instance.itemList[eventData.pointerDrag.GetComponent<ItemDragHandler>().transform.parent.GetSiblingIndex()];
+        GameObject obj = GameObject.Find("Level/Foreground/" + droppedItem.itemName);
+
+        RunDialogue dia = obj.GetComponent<RunDialogue>();
+        dia.runDialogue = false;
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray, out hit))
+        {
+            obj.SetActive(true);
+            obj.transform.position = hit.point;
+
+            Inventory.instance.RemoveItem(droppedItem);
+            Inventory.instance.UpdateSlotUI();
+
+            puzzle.dic[droppedItem.itemName] = true;
+            puzzle.checkIfComplete();
+        }
+    }
+}
