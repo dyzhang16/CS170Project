860790a1720a69614a07a7983fd0bb3d89972b6c
Ly Phung <lynphung@ucsc.edu>
2021-02-04 22:19:32 -0800

    changed blackscreen transition so its more intuitive

---
+++ Assets/Scripts/Menu/Transitions.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Yarn.Unity;
+
+public class Transitions : MonoBehaviour
+{
+
+    public Animator transition;
+    public float transitionSpeed = 1f;
+
+    [YarnCommand("ChangeScene")]
+    public void ChangeScene(string sceneToChange){
+        LoadNextScene(sceneToChange);
+    }
+
+    public void ExitGame(){
+        Application.Quit();
+    }
+
+    public void LoadNextScene(string sceneToChange){
+        StartCoroutine(FadeScene(sceneToChange));
+    }
+
+    IEnumerator FadeScene(string sceneToChange)
+    {
+        transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionSpeed);
+
+        SceneManager.LoadScene(sceneToChange);
+    }
+}
