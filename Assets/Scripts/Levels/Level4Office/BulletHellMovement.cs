3a9c55697fb83189d2728358e942591a0ad994b4
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2021-02-05 22:03:11 -0800

    Edited Logic For Document Puzzle
    Fixed the Logic regarding Document Puzzle
    Began Implementation for Computer Puzzle

---
+++ Assets/Scripts/Levels/Level4Office/BulletHellMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHellMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+    public GameObject player;
+    public SpriteRenderer render;
+    public bool moving = false;
+
+    public bool allowMovement = true;
+    public Vector2 movement = new Vector2(0, 0);
+    public void Update()
+    {
+        if (allowMovement)
+        {
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+
+            if (h != 0 || v != 0)
+            {
+                movement.y = v;
+                movement.x = h;
+                moving = true;
+
+                //animantor.SetFloat("Speed", Mathf.Abs(v) + Mathf.Abs(h));
+
+                if (h > 0)
+                {
+                    render.flipX = true;
+                }
+                else
+                {
+                    render.flipX = false;
+                }
+            }
+            else
+            {
+                moving = false;
+                movement = new Vector2(0, 0);
+            }
+        }
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/Levels/Level4Office/mouseOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class mouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [HideInInspector] public bool isMouseOver;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isMouseOver = true;
+        //Debug.Log("Mouse is Over");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isMouseOver = false;
+        //Debug.Log("Mouse is Exit");
+    }  
+}
