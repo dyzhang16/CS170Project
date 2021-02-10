2834845c6e7c784c4ed611fb1cdca3f329a86a47
Emmanuel Butor <ebutor@ucsc.edu>
2021-02-09 23:30:19 -0800

    Implemented multiple speakers and speaker focusing!
    Note: This is only in the scene named "DialogueUpdateScene.unity". The
    next to do is to find a way to merge this into the DiaSystem prefab.
    
    Usage of new/updated yarn commands:
    <<ChangeSpeaker P1 P2>>
    - dialogue with two speakers: P1 and P2.
    
    <<Focus P1>>
    - Focus on P1 (this requires "ChangeSpeaker P1" to have been called)
    
    <<Focus FIRST|SECOND|BOTH|NONE>>
    - Focus on the speaker on the left|speaker on the right|both
    speakers|neither speaker.
    
    ChangeSpeaker.cs
    - Added another command here, called Focus. The main reason it was added
    here was because of how much it would be needed with ChangeSpeaker.cs
    
    DialogueUpdateScene.unity, temp.yarn
    - contains the test dialogue

--- Assets/Scripts/Dialogue/ChangeSpeaker.cs
+++ Assets/Scripts/Dialogue/ChangeSpeaker.cs
@@ -7,8 +7,13 @@ using Yarn.Unity;
 public class ChangeSpeaker : MonoBehaviour
 {
     public DialogueRunner dialogueRunner;
-    public GameObject CharacterImage;
+    public GameObject CharacterImage; // character on the left
+    public GameObject SecondCharacterImage; // character on the right
     public GameObject TalkingCharacter;
+    public GameObject SecondTalkingCharacter;
+
+    // Dictionary for keeping track of character names (for focus)
+    private Dictionary<string, UnityEngine.UI.Image> focusPairs; // maps character names to Images
 
     public void Awake()
     {
@@ -18,19 +23,112 @@ public class ChangeSpeaker : MonoBehaviour
             "ChangeSpeaker",     // the name of the command
             ChangeImage // the method to run
         );
+        // New YarnCommand for specifying focus
+        dialogueRunner.AddCommandHandler(
+            "Focus",
+            SetFocus
+        );
+
+        // initialize focusPairs
+        focusPairs = new Dictionary<string, UnityEngine.UI.Image>();
     }
 
     private void ChangeImage(string[] parameters)
     {
-        string Person = parameters[0];
-        TalkingCharacter = GameObject.Find(Person);
-        if (TalkingCharacter == null)
+        // reset focusPairs
+        focusPairs.Clear();
+
+        // Inner function to change the image
+        Action<string, GameObject, GameObject> DoChange = (string person, GameObject talkingCharacter, GameObject characterImage) =>
         {
-            Debug.Log($"Cannot Find {TalkingCharacter}");
-            return;
+            string Person = person;
+            talkingCharacter = GameObject.Find(person);
+            if (talkingCharacter == null)
+            {
+                Debug.Log($"Cannot Find {talkingCharacter}");
+                return;
+            }
+            Debug.Log($"{talkingCharacter}: is talking.");
+            characterImage.GetComponent<UnityEngine.UI.Image>().sprite = talkingCharacter.GetComponent<SpriteRenderer>().sprite;
+        };
+
+        // ChangeImage for first character
+        DoChange(parameters[0], TalkingCharacter, CharacterImage);
+        focusPairs.Add(parameters[0], CharacterImage.GetComponent<UnityEngine.UI.Image>()); // map character name to image
+
+        // ChangeImage for second parameter (if it exists)
+        if (parameters.Length >= 2)
+        {
+            // Show the second character image if it does exist
+            SecondCharacterImage.SetActive(true);
+            DoChange(parameters[1], SecondTalkingCharacter, SecondCharacterImage);
+            focusPairs.Add(parameters[1], SecondCharacterImage.GetComponent<UnityEngine.UI.Image>()); // map character name to image
         }
-        Debug.Log($"{TalkingCharacter}: is talking.");
-        CharacterImage.GetComponent<UnityEngine.UI.Image>().sprite = TalkingCharacter.GetComponent<SpriteRenderer>().sprite;
+        else
+        {
+            // Hide the second character image if it doesn't exist
+            SecondCharacterImage.SetActive(false); // Hide the second character image
+        }
     }
 
+    private void SetFocus(string[] parameters)
+    {
+        // constants
+        Color IN_FOCUS = Color.white;
+        Color NOT_FOCUS = new Color(0.2f, 0.2f, 0.2f, 1);
+
+        // check for valid parameter count
+        if (parameters.Length == 0)
+        {
+            Debug.LogError("\"Focus\" must be called with either the name of the character, or" +
+                " one of the following: FIRST, SECOND, BOTH, NONE.");
+            return;
+        }
+
+        // check the argument for this command
+        switch (parameters[0])
+        {
+            case "FIRST":
+                CharacterImage.GetComponent<UnityEngine.UI.Image>().color = IN_FOCUS;
+                if (SecondCharacterImage.activeInHierarchy) // if second character image is active, set color to out of focus
+                {
+                    SecondCharacterImage.GetComponent<UnityEngine.UI.Image>().color = NOT_FOCUS;
+                }
+                break;
+            case "SECOND":
+                if (!SecondCharacterImage.activeInHierarchy)
+                {
+                    Debug.LogError("Focus: Cannot set focus to second image if there is no second speaker");
+                    return;
+                }
+                CharacterImage.GetComponent<UnityEngine.UI.Image>().color = NOT_FOCUS;
+                SecondCharacterImage.GetComponent<UnityEngine.UI.Image>().color = IN_FOCUS;
+                break;
+            case "BOTH":
+                CharacterImage.GetComponent<UnityEngine.UI.Image>().color = IN_FOCUS;
+                if (SecondCharacterImage.activeInHierarchy)
+                {
+                    SecondCharacterImage.GetComponent<UnityEngine.UI.Image>().color = IN_FOCUS;
+                }
+                break;
+            case "NONE":
+                CharacterImage.GetComponent<UnityEngine.UI.Image>().color = NOT_FOCUS;
+                if (SecondCharacterImage.activeInHierarchy)
+                {
+                    SecondCharacterImage.GetComponent<UnityEngine.UI.Image>().color = NOT_FOCUS;
+                }
+                break;
+            default: // When a name is provided
+                // just reuse SetFocus to unfocus everything
+                string[] paramsCopy = new string[1];
+                paramsCopy[0] = "NONE";
+                SetFocus(paramsCopy);
+                // then, set focus based on parameters[0] (which should be a name)
+                if (!focusPairs.ContainsKey(parameters[0]))
+                    Debug.LogError($"Could not find name: {parameters[0]}");
+                else
+                    focusPairs[parameters[0]].color = IN_FOCUS;
+                break;
+        }
+    }
 }
---
+++ Assets/Scripts/Levels/DialogueUpdateScene/README.txt
@@ -0,0 +1,2 @@
+This folder ("Assets/Scripts/Levels/DialogueUpdateScene/") holds temporary assets
+for the scene DialogueUpdateScene (located in "Assets/Scenes").
