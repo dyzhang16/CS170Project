3e79f9ecc6070388d3fd857d88fbd37b99937f4d
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2021-02-19 20:30:56 -0800

    Edited Dialogue System

---
+++ Assets/Scripts/Levels/Level4Office/OfficeHub/Script/OfficeFriend.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class OfficeFriend : MonoBehaviour
+{
+    public GameObject friend;
+    [YarnCommand("Move")]
+    public void Move()
+    {
+        friend.transform.position = new Vector3(0,0,0);
+    }
+}
