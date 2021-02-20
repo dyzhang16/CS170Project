72477c759f5d0ce298fee75dc5d42039cdf1a12d
Emmanuel Butor <ebutor@ucsc.edu>
2021-02-19 22:32:40 -0800

    Added more crane prizes, and modified some dialogue, and made it so that the exit condition is to try every game before leaving. When you leave via the gate, the game closes

---
+++ Assets/Scripts/Levels/Level6Arcade/TempExitGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class TempExitGame : MonoBehaviour
+{
+	DialogueRunner dialogueRunner;
+	void Awake()
+	{
+		dialogueRunner = GameObject.FindObjectOfType<DialogueRunner>();
+	}
+	void Start()
+	{
+		dialogueRunner.AddCommandHandler(
+			"ExitGame",
+			(string[] args) =>
+			{
+				Debug.Log("Game Exited");
+				Application.Quit();
+			}
+		);
+	}
+}
