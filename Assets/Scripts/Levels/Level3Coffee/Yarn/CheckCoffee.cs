c0d1bdbff6ff0f8f538c6cea1115157dd4333581
Ly Phung <lynphung@ucsc.edu>
2021-02-19 23:22:39 -0800

    sorta fixed coffee scene

--- Assets/Scripts/Levels/Level3Coffee/Yarn/CheckCoffee.cs
+++ Assets/Scripts/Levels/Level3Coffee/Yarn/CheckCoffee.cs
@@ -8,6 +8,7 @@ public class CheckCoffee : MonoBehaviour , IDropHandler
 {
     public GameObject puzzlePanel;
     public VariableStorageBehaviour CustomVariableStorage;
+    public DialogueRunner dia;
 
     public void OnDrop(PointerEventData eventData)
     {
@@ -20,6 +21,7 @@ public class CheckCoffee : MonoBehaviour , IDropHandler
                 CustomVariableStorage.SetValue("$CoffeePuzzle", 1);
                 puzzlePanel.GetComponent<CanvasGroup>().alpha = 0;
                 puzzlePanel.GetComponent<CanvasGroup>().blocksRaycasts = false;
+                GameManager.instance.coffeePuzzle = 1;
                 // allow background interactions
                 GameObject blocker = GameObject.Find("Canvas/Blocker");
                 if (blocker)
@@ -28,6 +30,8 @@ public class CheckCoffee : MonoBehaviour , IDropHandler
                     blocker.GetComponent<CanvasGroup>().blocksRaycasts = false;
                 }
                 Inventory.instance.RemoveItem(droppedItem);
+
+                dia.StartDialogue("CorrectCoffee");
             }
             else 
             {
