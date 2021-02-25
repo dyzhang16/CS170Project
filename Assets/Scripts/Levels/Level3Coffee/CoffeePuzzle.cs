7c015f131fad33281b6717b2300ab7226fe4624e
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2021-02-24 21:44:39 -0800

    Updated Dialogue Command to Reflect Characters Images on Right Side Only
    Use <<ChangeSpeaker None (Character)>> to only use the second picture
    Edited Coffee Level

--- Assets/Scripts/Levels/Level3Coffee/CoffeePuzzle.cs
+++ Assets/Scripts/Levels/Level3Coffee/CoffeePuzzle.cs
@@ -20,7 +20,6 @@ public class CoffeePuzzle : MonoBehaviour, IDropHandler
         cupThere = false;
         filterThere = false;
         groundsThere = false;
-        waterThere = false;
         brewedCoffee.SetActive(false);
     }
 
