d80187756e6feec0597cef7e9aa53abe2f21ab51
dyzhang16 <63315719+dyzhang16@users.noreply.github.com>
2021-02-25 15:33:34 -0800

    Added Transitions between Street/Office Hub/Office Scene

---
+++ Assets/Scripts/Levels/Level4Office/OfficeHub/Script/OfficeHub.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class OfficeHub : MonoBehaviour
+{
+    public DialogueRunner dialogueRunner;
+
+    void Awake()
+    {
+        if (GameManager.instance != null)
+        {
+
+        }
+    }
+    void Start()
+    {
+
+    }
+}
