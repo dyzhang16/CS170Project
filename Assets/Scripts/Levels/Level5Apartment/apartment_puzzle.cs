dd8b8feaa5b246725ede5c62606bd40dfad0ebca
Ly Phung <lynphung@ucsc.edu>
2021-04-09 15:14:08 -0700

    added return to main menu

--- Assets/Scripts/Levels/Level5Apartment/apartment_puzzle.cs
+++ Assets/Scripts/Levels/Level5Apartment/apartment_puzzle.cs
@@ -9,8 +9,16 @@ public class apartment_puzzle : MonoBehaviour
     public GameObject[] packingList = new GameObject[0];
     public Dictionary<string, bool> dic = new Dictionary<string, bool>();
 
+    public GameObject[] placeHeres = new GameObject[0];
+    
+    public GameObject[] escapeRoomItems = new GameObject[0];
+
     public bool puzzleComplete;
     public VariableStorageBehaviour CustomVariableStorage;
+    public DialogueRunner dia;
+
+    public GameObject friend;
+    public GameObject bed;
 
     // Start is called before the first frame update
     void Start()
@@ -47,10 +55,40 @@ public class apartment_puzzle : MonoBehaviour
 
     [YarnCommand("finishedLevel")]
     public void finishedLevel(){
-        //change all dialogue to runs of objs
+
+        //add delay to make the transition better
+        StartCoroutine(finishedCleaning());
+
+        //add all new items to scene
+        
 
         //change dialogeu to run of friend
+        friend.GetComponent<RunDialogue>().dialogueToRun = "";
+
+        //change dialogue of bed
+        bed.AddComponent<RunDialogue>();
 
         //remove all placeheres
+        foreach (GameObject obj in placeHeres) {
+            Destroy(obj);
+        }
+    }
+
+    IEnumerator finishedCleaning(){
+        yield return new WaitForSeconds(1f);
+
+        //change color back
+        foreach (GameObject obj in packingList) {
+            obj.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
+            if (obj.name != "Bookshelf") {
+                Destroy(obj.GetComponent<RunDialogue>());
+            } else {
+                obj.GetComponent<RunDialogue>().dialogueToRun = "BookshelfEscape";
+            }
+        }
+
+        yield return new WaitForSeconds(1f);
+        //start first friend dialogue
+        dia.StartDialogue("first_friend_after_cleaning");
     }
 }
