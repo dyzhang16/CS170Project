3df5387edcb0077c0856eda60304b8ce34f8e88b
Emmanuel Butor <ebutor@ucsc.edu>
2021-04-28 22:56:58 -0700

    Updated the ending cutscene to have a bit more detail

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -219,6 +220,70 @@ public class Player : MonoBehaviour               //https://stackoverflow.com/qu
     //     //if(col.gameObject.tag == "wall")
     //     SoundManagerScript.PlaySound("wall_bump");
     // }
+
+    // Moves player to location (simulated walking)
+    //      Parameters: [0] = location name, [1-3] = offset as numbers
+    [YarnCommand("MovePlayer")]
+    public void MovePlayer(string[] parameters)
+    {
+        // use better variable names for the parameters
+        string locationName = parameters[0];
+        string[] offset = new string[parameters.Length - 1];
+        Array.Copy(parameters, 1, offset, 0, offset.Length);
+
+        // get the GameObject that the location is referring to
+        GameObject location = GameObject.Find(locationName);
+
+        // if the location is null (i.e. the GameObject was not found), log a warning and do no movement
+        if (location == null)
+        {
+            Debug.LogWarningFormat("MovePlayer: Could not find GameObject {0}. Player movement was not done.", locationName);
+        }
+
+        // calculate the target position + offset
+
+        // target is first based off the location
+        Vector3 targetPosition = location.transform.position;
+
+        // turn the offset (string array) into floats
+        
+        if (offset.Length > 3)
+        {
+            Debug.LogWarning("MovePlayer: Too many arguments, only considering the x, y, z arguments");
+        }
+
+        // for-loop up to three offset arguments
+        for (int i = 0; i < Mathf.Min(offset.Length, 3); i++)
+        {
+            try
+            {
+                // convert string to float and apply offset
+                targetPosition[i] += float.Parse(offset[i]);
+            }
+            catch (System.FormatException exc)
+            {
+                // array for debug logging purposes
+                char[] offsetChar = new char[] { 'x', 'y', 'z' };
+                Debug.LogWarningFormat("MovePlayer: Failed to apply offset \"{0}\" for {1} position.", offset[i], offsetChar[i]);
+            }
+        }
+
+        // Move the player to the target location (with the offset)
+        StartCoroutine(MovePlayerCR(targetPosition));
+    }
+
+    // Helper Coroutine for use with MovePlayer()
+    IEnumerator MovePlayerCR(Vector3 targetPosition)
+    {
+        // loop movement until the player is at the target position
+        while (transform.position != targetPosition)
+        {
+            // move the player via its transform
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+            yield return null;
+        }
+    }
 }
 
 
