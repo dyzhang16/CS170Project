f2e1aafac4901aa793de585757b79f69e2c44172
Emmanuel Butor <ebutor@ucsc.edu>
2021-05-04 00:31:58 -0700

    UnityRichTextWriter.cs bugfix: force completing the dialogue will now correctly display all dialogue

--- Assets/Scripts/Dialogue/UnityRichTextWriter.cs
+++ Assets/Scripts/Dialogue/UnityRichTextWriter.cs
@@ -21,7 +21,7 @@ public class UnityRichTextWriter : MonoBehaviour
 
 	// Methods
 
-	// sets the text for the RichTextWriter
+	// sets the text for the RichTextWriter (called in On Line Start)
 	public void SetText(string text)
 	{
 		// set the RichTextWriter up
@@ -32,40 +32,51 @@ public class UnityRichTextWriter : MonoBehaviour
 		index = 0;
 	}
 
-	// displays the next character
+	// displays the next character (called in On Line Update)
 	public void ShowNextCharacter(string request)
 	{
+		// if Text is broken, don't run the function
+		if (dialogueText == null) return;
+
 		// if the writer has no text, do default behavior
 		if (writer.parsedString.Count <= 0)
 		{
 			dialogueText.text = request;
+			return;
 		}
-		// if Text is broken, don't run the function
-		if (dialogueText == null) return;
+
+		// if the request is the entire string, then just display the entire string
+		if (request == writer.parsedString[writer.parsedString.Count - 1])
+		{
+			FinishDisplayingText();
+		}
 		// if index is less than parsedString, then use the parsedString for the dialogue text
-		if (index < writer.parsedString.Count)
+		else if (index < writer.parsedString.Count)
 		{
 			// update the dialogue text
-			dialogueText.text
-				= writer.parsedString[index];
+			dialogueText.text = writer.parsedString[index];
 
 			// move the index up
 			index++;
 		}
-		// otherwise, use the last item in the parsedString
+		// otherwise, use the last item in the parsedString and indicate that the line is complete
 		else if (writer.parsedString.Count > 0)
 		{
 			dialogueText.text = writer.parsedString[writer.parsedString.Count - 1];
 
-			// also invoke the on line finished updating 
+			// also invoke the on line finished updating to prevent more sound from playing
 			DialogueUI dialogueUI = GameObject.Find("DialogueRunner").GetComponent<DialogueUI>();
 			dialogueUI.MarkLineComplete();
 		}
+		
 	}
 
 	// finish displaying text
 	public void FinishDisplayingText()
 	{
-
+		if (writer.parsedString.Count > 0)
+		{
+			dialogueText.text = writer.parsedString[writer.parsedString.Count - 1];
+		}
 	}
 }
